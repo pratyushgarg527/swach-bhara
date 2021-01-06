a08f039e8f90350679e6309132bd99a7d96e1934
pratyushgarg527 <73683956+pratyushgarg527@users.noreply.github.com>
2021-01-06 12:57:27 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -10,7 +10,7 @@ class Dustbin {
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
-      this.image = loadImage("sprites/dustbingreen.png");
+      this.image = loadImage("dustbingreen.png");
       
       World.add(world, this.body);
     }
